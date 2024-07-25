fc580f1136ba6f524ae6a6f2b2b526d8efb16cb9
Andrew Crisp <andrewwcrisp@gmail.com>
2024-07-25 19:03:51 +0200

    trying simple init

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 from flask import Flask
+import ee
 
-import urllib
-import google.auth.transport.requests
-import google.oauth2.id_token
-
-endpoint = os.environ.get('ENDPOINT')
-audience = os.environ.get('AUDIENCE')
-
-def make_authorized_get_request(endpoint, audience):
-    """
-    make_authorized_get_request makes a GET request to the specified HTTP endpoint
-    by authenticating with the ID token obtained from the google-auth client library
-    using the specified audience value.
-    """
-
-    # Cloud Run uses your service's hostname as the `audience` value
-    # audience = 'https://my-cloud-run-service.run.app/'
-    # For Cloud Run, `endpoint` is the URL (hostname + path) receiving the request
-    # endpoint = 'https://my-cloud-run-service.run.app/my/awesome/url'
-
-    req = urllib.request.Request(endpoint)
-
-    auth_req = google.auth.transport.requests.Request()
-    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
-
-    req.add_header("Authorization", f"Bearer {id_token}")
-    response = urllib.request.urlopen(req)
 
 
 
@@ -35,5 +10,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print("attempting...")
-    print(make_authorized_get_request)
+    ee.Initialize(project='gee-playground-project')
     return 'Hello, Worlds!'
