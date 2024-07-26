652caadfc4beea73d310e3f9e7361c28aa76509f
Andrew Crisp <andrewwcrisp@gmail.com>
2024-07-26 10:29:26 +0200

    updated init

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask
 import ee
 
-
-
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("attempting...")
-    ee.Initialize(project='gee-playground-project')
+    ee.Initialize()
+    
+
     return 'Hello, Worlds!'
